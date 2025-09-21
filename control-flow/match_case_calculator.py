a2d72a0732d53205d0400ab5fd073d0be95fa15f
Robel Alemayehu <robelalemayehu838@gmail.com>
2025-09-21 14:09:26 +0300

    feat:add a python program that performs calculatio

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,24 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
+    case "+":
+        result = num1 + num2
+    case "-":
+        result = num1 - num2
+    case "*":
+        result = num1 * num2
+    case "/":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            print("The denominator can not be 0")
+    case _:
+        print("Invalid operation")
+        result = None
+
+print("The result is", result)
+        
+    
+    

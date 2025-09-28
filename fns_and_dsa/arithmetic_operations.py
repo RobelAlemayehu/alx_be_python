22eb1267574bd6b8b7003dee8eb09944f3358f04
Robel Alemayehu <robelalemayehu838@gmail.com>
2025-09-28 21:01:42 +0300

    a python function that performs simple arthimetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,14 @@
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "substruct":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: num2 can not be 0"
+        return num1 / num2
+    else:
+        return "Invalid Operation"
+    

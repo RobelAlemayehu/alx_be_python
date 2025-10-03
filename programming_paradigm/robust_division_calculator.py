c4971f7d3bdfd9c97caccc7c8753a8ed3285064d
Robel Alemayehu <robelalemayehu838@gmail.com>
2025-10-03 22:47:49 +0300

    fix: typo error

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,7 +4,7 @@ def safe_divide(numerator, denominator):
     
     try:
        result = float(numerator) / float(denominator)
-       return f"The result of the dvision is {result}"
+       return f"The result of the division is {result}"
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:

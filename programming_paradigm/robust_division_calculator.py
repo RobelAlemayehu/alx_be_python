0a5a7ee73663d945906c85cb99393d00efc44009
Robel Alemayehu <robelalemayehu838@gmail.com>
2025-10-03 20:58:04 +0300

    feat: add a python program that  used as a division calculator

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,14 @@
+
+
+def safe_divide(self,numerator, denominator):
+    self.numerator = numerator
+    self.denominator = denominator
+    
+    try:
+       value = numerator / denominator 
+    except ZeroDivisionError:
+        print("Denominator can not be zero")
+    except ValueError:
+        print("Value can not be non numeric character")    
+    
+        
